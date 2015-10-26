76627a580a7aebcbd558a2c85ca15834ceef347f
Serhii-Danovsky <jipebed@gmail.com>
2015-10-26 15:35:27 +0200

    add download

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ActiveRecord::Base
   validates :title, uniqueness: true
   validates :title, length: { in: 5..100 }
 
+def self.to_csv(options = {})
+  CSV.generate(options) do |csv|
+    csv << column_names
+    all.each do |post|
+      csv << post.attributes.values_at(*column_names)
+    end
+  end
+end
 end
