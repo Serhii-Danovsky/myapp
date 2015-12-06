f7143def7bab6a4556ef968ed85932729a6f2204
Serhii-Danovsky <jipebed@gmail.com>
2015-12-06 23:21:24 +0200

    tooltip add

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,3 +23,6 @@ $(document).ready(function() {
   /* Activating Best In Place */
   jQuery(".best_in_place").best_in_place();
 });
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
