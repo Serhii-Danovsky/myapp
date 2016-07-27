eb8e2be14118833dc9675eead6b5254006165e73
Serhii-Danovsky <jipebed@gmail.com>
2016-07-27 09:23:57 +0300

    Signed-off-by: Serhii-Danovsky <jipebed@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_filter :set_access
   def current_user
     current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+
+
+  def set_access
+    @response.headers["Access-Control-Allow-Origin"] = "*"
+  end
+
 
   def favorite_post?(post)
     current_user.favorite_posts.where(post_id: post.id).present?
