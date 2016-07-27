8afe17859d6077fcb4af80a3c947db568d7d3da5
Serhii-Danovsky <jipebed@gmail.com>
2016-07-27 11:57:05 +0300

    Signed-off-by: Serhii-Danovsky <jipebed@gmail.com> pass

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -3,12 +3,14 @@ class DemosController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create_demo_user
+    pass = rand(100000..999999)
     name_json = params[:name]
     email_json = params[:email]
-    @resource = User.new({:email => email_json, :password => "111111", :password_confirmation => "111111"  , :name => name_json})
-
+    @resource = User.new({:email => email_json, :password => pass, :password_confirmation => pass , :name => name_json})
+    response = @resource.as_json
+    response['password'] = pass
     if @resource.save
-      render json: @resource , status: :ok
+      render json: response  , status: :ok
     else
       render json: @resource.errors, status: :unprocessable_entity
     end
