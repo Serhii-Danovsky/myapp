437d34eeb872af7483130c0305abcdb0ef8e8ac4
Serhii-Danovsky <jipebed@gmail.com>
2016-07-27 10:12:29 +0300

    Signed-off-by: Serhii-Danovsky <jipebed@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  skip_before_action :verify_authenticity_token, if: :json_request?
   def current_user
     current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -13,6 +13,10 @@ class ApplicationController < ActionController::Base
     User.create!({:email => "#{rand(1..9999)}guy@gmail.com", :password => "111111", :password_confirmation => "111111"  , :name => name_json})
   end
 
+
+  def json_request?
+    request.format.json?
+  end
 
   def favorite_post?(post)
     current_user.favorite_posts.where(post_id: post.id).present?
